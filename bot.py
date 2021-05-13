f4cf27315aa49e5aec2b81ca556c5135ba14a9a4
Sofia Berlin <sofiamberlin@gmail.com>
2021-05-13 15:39:46 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+from discord.ext import commands
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# Required to access list of members. Must also check boxes in bot permmissions
+# intents = discord.Intents.all()
+# client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the Goldfire server!'
+    )
+
+bot.run(TOKEN)
