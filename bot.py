f8d30c1b342a288e4b4de8b4360a7cad597bb810
Sofia Berlin <sofiamberlin@gmail.com>
2021-05-24 13:50:47 -0700

    Testing a new Bot class

--- bot.py
+++ bot.py
@@ -22,42 +22,45 @@ GUILD = os.getenv('DISCORD_GUILD')
 if 'gatherings' not in db:
     db['gatherings'] = []
 
-bot = commands.Bot(command_prefix='!')
+# bot = commands.bot(command_prefix='!')
 
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!')
+
+    @super().event
+    async def on_ready():
+        print(f'{bot.user.name} has connected to Discord!')
+
+    @super().command(name='list', help='Lists all Circles')
+    async def list_gatherings(ctx):
+        if len(db['gatherings']) == 0:
+            await ctx.send('There are no Circles scheduled')
+        else:
+            gatherings = [
+                Gathering(gathering_data) for gathering_data in db['gatherings']
+            ]
+            await ctx.send('\n'.join(gatherings))
+
+    @super().command(name='list open', help='Lists circles with availability')
+    async def list_open_gatherings(ctx):
+        if len(db['gatherings']) == 0:
+            await ctx.send('There are no Circles scheduled')
+        else:
+            all_gatherings = [
+                Gathering(gathering_data) for gathering_data in db['gatherings']
+            ]
+            available_gatherings = list(
+                filter(lambda gathering: (gathering.isOpen()), all_gatherings)
+            )
+            await ctx.send('\n'.join(available_gatherings))
+
+    @super().command(name='create', help='Create a new Circle')
+    async def create_gathering(ctx):
+        # await list_open_gatherings(ctx)
+        await ctx.send('testing')
 
 
-@bot.command(name='list', help='Lists all Circles')
-async def list_gatherings(ctx):
-    if len(db['gatherings']) == 0:
-        await ctx.send('There are no Circles scheduled')
-    else:
-        gatherings = [
-            Gathering(gathering_data) for gathering_data in db['gatherings']
-        ]
-        await ctx.send('\n'.join(gatherings))
-
-
-@bot.command(name='list open', help='Lists circles with availability')
-async def list_open_gatherings(ctx):
-    if len(db['gatherings']) == 0:
-        await ctx.send('There are no Circles scheduled')
-    else:
-        all_gatherings = [
-            Gathering(gathering_data) for gathering_data in db['gatherings']
-        ]
-        available_gatherings = list(
-            filter(lambda gathering: (gathering.isOpen()), all_gatherings)
-        )
-        await ctx.send('\n'.join(available_gatherings))
-
-
-@bot.command(name='create', help='Create a new Circle')
-async def create_gathering(ctx):
-    await list_open_gatherings(ctx)
-    await ctx.send('testing')
-
+bot = Bot()
 bot.run(TOKEN)
