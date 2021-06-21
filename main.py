e9b77c06d126113f859f4079a59f7c9172ec0cb0
Sofia Berlin <sofiamberlin@gmail.com>
2021-06-21 14:55:50 -0700

    Make delete command

--- main.py
+++ main.py
@@ -130,6 +130,55 @@ async def join_gathering(ctx):
         await ctx.send(f'Error: {str(e)}')
 
 
+@bot.command(name='delete', help='Delete one of your Circles')
+async def delete_gathering(ctx):
+    def check(message):
+        if (message.author == ctx.author) and (message.channel == ctx.channel):
+            return True
+        else:
+            return False
+    try:
+        if not db['gatherings']:
+            await ctx.send('No Gatherings')
+            return
+        gatherings = []
+        for gathering_data in db['gatherings']:
+            gathering = Gathering(gathering_data)
+            if gathering.author.id == ctx.author.id:
+                gatherings.append(gathering)
+        if not gatherings:
+            await ctx.send('You did not create any gatherings')
+            return
+        # Author created only 1 gathering:
+        if len(gatherings) == 1:
+            selected_gathering = gatherings[0]
+        # Author created multiple gatherings
+        else:
+            await ctx.send(
+                'Select the Index of the Circle to delete:\n{0}'
+                .format(bot.pprint_gatherings(gatherings))
+            )
+            msg = await bot.wait_for('message', check=check)
+            index = util.str_to_int(msg.content)
+            if (index > len(gatherings) - 1) or index < 0:
+                raise ValueError('Number is out of range')
+            selected_gathering = gatherings[index]
+        # Delete Selected Gathering:
+        await ctx.send(
+            'Do you wish to delete this gathering? Type y or n\n{0}'
+            .format(selected_gathering.toString())
+        )
+        msg = await bot.wait_for('message', check=check)
+        if msg.content not in msg.content['y', 'n']:
+            raise ValueError('Type y or n only')
+        if msg.content == 'y':
+            db['gatherings'].remove(selected_gathering.toJSON())
+            await ctx.send('Circle Deleted')
+            return
+    except (ValueError, IndexError) as e:
+        await ctx.send(f'Error: {str(e)}')
+
+
 # Delete all messages that don't start with '!'
 @bot.event
 async def on_message(message):
